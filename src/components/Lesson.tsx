635aa03991f11ff16111c865d775e2673427ef75
Gustavo Azevedo <gustavosazevedo@gmail.com>
2022-06-26 19:14:19 -0300

    App

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,65 @@
+import { CheckCircle, Lock } from 'phosphor-react';
+import { isPast, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+
+export function Lesson({ title, slug, availableAt, type }: LessonProps) {
+  const { slug: paramsSlug } = useParams<{ slug: string }>();
+
+  const isLessonAvailable = isPast(availableAt);
+  const availableDateFormatted = format(availableAt, "EEEE ' • ' d ' de ' MMMM ' • ' k'h'mm ", {
+    locale: ptBR
+  })
+
+  const isActiveLesson = slug === paramsSlug;
+
+  return (
+    <Link to={`/event/lesson/${slug}`} className='group'>
+      <span className="text-gray-300">
+        {availableDateFormatted}
+      </span>
+      <div className={classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors', {
+        'bg-green-500': isActiveLesson,
+      })}>
+        <header className="flex items-center justify-between">
+          {isLessonAvailable ? (
+            <span className={classNames('flex items-center gap-2 text-sm font-medium', {
+              'text-white': isActiveLesson,
+              'text-blue-500': !isActiveLesson,
+            })}>
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+
+          <span className={classNames('text-xs rounded px-2 py-[0.125rem] text-white border font-bold transition-colors', {
+            'border-white': isActiveLesson,
+            'border-green-500': !isActiveLesson,
+          })}>
+            {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+          </span>
+        </header>
+
+        <strong className={classNames('mt-5 block', {
+          'text-white': isActiveLesson,
+          'text-gray-200': !isActiveLesson,
+        })}>
+          {title}
+        </strong>
+      </div>
+    </Link>
+  )
+}
